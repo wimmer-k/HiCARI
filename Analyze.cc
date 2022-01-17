d209cd1f07d0323fcae5dd3ca0334c3d1e1779f5
wimmer-k <k.wimmer@gsi.de>
2022-01-17 19:39:44 +0100

    target position reference fixed

--- Analyze.cc
+++ Analyze.cc
@@ -214,7 +214,6 @@ int main(int argc, char* argv[]){
 
     // target position with respect to the nominal focal point
     TVector3 targ = rec->TargetPosition(inc,ppacpos[1]);
-    targ.SetZ(set->TargetZ());
     bz->SetTargetPosition(targ);
     TVector3 out, sca;
     if(trigbit>1){
@@ -223,6 +222,9 @@ int main(int argc, char* argv[]){
       sca = bz->GetScatteredDirection();
     }
 
+    // change target position with respect to HiCARI center
+    targ.SetZ(set->TargetZ());
+    bz->SetTargetPosition(targ);
     //beta
     //bz->SetDopplerBeta(set->TargetBeta());
     //bz->SetDopplerBeta(bz->GetRIPSBeta(3));
